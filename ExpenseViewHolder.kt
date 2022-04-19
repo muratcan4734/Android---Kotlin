836f41ea5a30f61aa43d55cea3947a4fc950ecc3
Murat CAN <49750865+muratcan4734@users.noreply.github.com>
2022-04-19 16:42:35 +0100

    Delete ExpenseViewHolder.kt

--- ExpenseViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.murat.expensestracker_assignment1
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class ExpenseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-}
