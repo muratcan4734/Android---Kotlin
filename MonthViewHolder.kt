ffa8ad4903425fbfb7675976bf3b9c60627af8ff
Murat CAN <49750865+muratcan4734@users.noreply.github.com>
2022-04-19 16:41:22 +0100

    Delete MonthViewHolder.kt

--- MonthViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.murat.expensestracker_assignment1
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class MonthViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-}
